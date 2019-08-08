47a686fdc68d01f2a83c9456f2e53c230dcef926
niEmerance <niyemera6@gmail.com>
2019-08-08 09:49:46 +0200

    make changes on paragraphs

--- js/script.js
+++ js/script.js
@@ -3,4 +3,13 @@ $(".one").click(function(){
     $("img#A").toggle();
     $("#first").toggle();
 });
+$(".two").click(function(){
+    $("img#B").toggle();
+    $("#second").toggle();
 });
+$(".three").click(function(){
+    $("img#C").toggle();
+    $("#third").toggle();
+});
+});
+
